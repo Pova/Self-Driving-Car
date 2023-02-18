1fd1c824d165265946c84cce708cb98d8e186762
Roman Kitsela <rkitsela@ucsd.edu>
2023-02-18 14:51:49 +0000

    added canvas element and control types

--- main.js
+++ main.js
@@ -1,23 +1,26 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const roadCanvas = document.getElementById('roadCanvas');
+roadCanvas.height=window.innerHeight * 0.9;
+roadCanvas.width=500;
+const roadCtx = roadCanvas.getContext('2d');
 
+const networkCanvas = document.getElementById('networkCanvas');
+networkCanvas.height=window.innerHeight * 0.7;
+networkCanvas.width=500;
+const networkCtx = networkCanvas.getContext('2d');
 
-
-canvas.height=window.innerHeight * 0.9;
-canvas.width=500;
-
-const centerx = canvas.width/2;
+const centerx = roadCanvas.width/2;
 const laneWidth = 40;
 const laneCount = 5;
+const controlType = 'AI'
 
-let car = new Player(centerx, 200, 20, 40);
+let car = new Player(centerx, 200, 20, 40, controlType);
 let path = new Path( centerx, 100, 400);
 let traffic = new Traffic();
 let play = true;
 
 mouse = {}
-canvas.addEventListener('click', (e) => {
-    car = new Player(centerx, 200, 20, 40);
+roadCanvas.addEventListener('click', (e) => {
+    car = new Player(centerx, 200, 20, 40, controlType);
     path = new Path( centerx, 100, 400);
     traffic = new Traffic();
     play = true;
@@ -27,16 +30,17 @@ animate();
 
 function animate(){
     if (play) {
-        canvas.height = window.innerHeight * 0.9;  // used to clear canvas
-        
+        roadCanvas.height = window.innerHeight * 0.9;  // used to clear canvas
+        networkCanvas.height = window.innerHeight * 0.9;
+
         car.update(path,traffic);
         path.update();
         traffic.update();
         
-        ctx.translate(0, -car.y + canvas.height * 0.8);
+        roadCtx.translate(0, -car.y + roadCanvas.height * 0.8);
         path.draw();    
         traffic.draw();
-        car.draw(ctx)
+        car.draw(roadCtx)
     }
     requestAnimationFrame(animate);
 }
