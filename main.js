86e24d47119a296f18da8268da1cfd41e734ff9d
Roman Kitsela <roman.kitsela@gmail.com>
2023-05-24 00:20:26 +0100

    Add support for multiple cars

--- main.js
+++ main.js
@@ -11,16 +11,21 @@ const networkCtx = networkCanvas.getContext('2d');
 const centerx = roadCanvas.width/2;
 const laneWidth = 40;
 const laneCount = 5;
-const controlType = 'AI'
+const controlType = 'AI';
 
-let car = new Player(centerx, 200, 20, 40, controlType);
+// Generate multiple cars
+const num = 100;
+let cars = generateCars(num);
+let bestCar = cars[0];
+
 let path = new Path( centerx, 100, 400);
 let traffic = new Traffic();
 let play = true;
 
 mouse = {}
 roadCanvas.addEventListener('click', (e) => {
-    car = new Player(centerx, 200, 20, 40, controlType);
+    cars = generateCars(num);
+    bestCar = cars[0];
     path = new Path( centerx, 100, 400);
     traffic = new Traffic();
     play = true;
@@ -28,20 +33,43 @@ roadCanvas.addEventListener('click', (e) => {
 
 animate();
 
-function animate(){
+function generateCars(num){
+    const cars = [];
+    for (let i=1; i<=num; i++){
+        cars.push(new Player(centerx, 200, 20, 40, controlType));
+    }
+    return cars
+}
+
+function animate(time){
     if (play) {
         roadCanvas.height = window.innerHeight * 0.9;  // used to clear canvas
         networkCanvas.height = window.innerHeight * 0.9;
 
-        car.update(path,traffic);
+        for (let i=0; i<cars.length; i++){
+            cars[i].update(path,traffic);
+        }
+        bestCar = cars.find(
+            c=>c.y==Math.min(
+                ...cars.map(c=>c.y)
+            ));
+
         path.update();
         traffic.update();
         
-        roadCtx.translate(0, -car.y + roadCanvas.height * 0.8);
+        roadCtx.translate(0, -bestCar.y + roadCanvas.height * 0.8);
         path.draw();    
         traffic.draw();
-        car.draw(roadCtx)
-        Visualizer.drawNetwork(networkCtx, car.brain);
+
+        roadCtx.globalAlpha=0.2;
+        for (let i=0; i<cars.length; i++){
+            cars[i].draw(roadCtx);
+        }
+        roadCtx.globalAlpha=1;
+        bestCar.draw(roadCtx,true);
+
+        networkCtx.lineDashOffset=-time/50;
+        Visualizer.drawNetwork(networkCtx, bestCar.brain);
     }
     requestAnimationFrame(animate);
 }
